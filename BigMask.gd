86e28653bd54729f17efcef43832fc640bfc8cc0
Miles Turin <31150941+milesturin@users.noreply.github.com>
2020-03-03 15:16:35 -0800

    Added extra print option

--- BigMask.gd
+++ BigMask.gd
@@ -2,7 +2,7 @@ class_name BigMask
 extends Reference
 
 const MASK_SIZE := 32
-var bit_masks: PoolIntArray = []
+var bitmasks: PoolIntArray = []
 var bits: int
 
 func _init(size: int, default_state: bool = false, enforce_soft_size: bool = true) -> void:
@@ -10,23 +10,29 @@ func _init(size: int, default_state: bool = false, enforce_soft_size: bool = tru
 
 func resize(size: int, default_state: bool = false, enforce_soft_size: bool = true) -> void:
 	assert(size >= 0)
-	var old_masks = bit_masks.size()
-	bit_masks.resize(ceil(size / float(MASK_SIZE)))
-	bits = size if enforce_soft_size else bit_masks.size() * MASK_SIZE
-	for i in range(old_masks, bit_masks.size()):
-		bit_masks[i] = ~0 if default_state else 0
+	var old_masks := bitmasks.size()
+	bitmasks.resize(ceil(size / float(MASK_SIZE)))
+	bits = size if enforce_soft_size else bitmasks.size() * MASK_SIZE
+	for i in range(old_masks, bitmasks.size()):
+		bitmasks[i] = ~0 if default_state else 0
 
 func check_bit(index: int) -> bool:
 	assert(index < bits)
-	return bit_masks[index / MASK_SIZE] & (1 << (index % MASK_SIZE)) != 0
+	return bitmasks[index / MASK_SIZE] & (1 << (index % MASK_SIZE)) != 0
 
 func set_bit(index: int, state: bool) -> void:
 	assert(index < bits)
 	if state:
-		bit_masks[index / MASK_SIZE] |= (1 << (index % MASK_SIZE))
+		bitmasks[index / MASK_SIZE] |= (1 << (index % MASK_SIZE))
 	else:
-		bit_masks[index / MASK_SIZE] &= ~(1 << (index % MASK_SIZE))
+		bitmasks[index / MASK_SIZE] &= ~(1 << (index % MASK_SIZE))
 
-func print_bits() -> void:
-	for i in range(bits):
-		print("bit " + String(i) + ": " + String(check_bit(i)))
+func print_bits(multiline: bool = true) -> void:
+	if multiline:
+		for i in range(bits):
+			print("bit " + String(i) + ": " + String(check_bit(i)))
+	else:
+		var output := ""
+		for i in range(bits):
+			output += '1' if check_bit(i) else '0'
+		print(output)
